e13e84d92f1afc6734164f68163d33042386dabf
sherotree <wytreeshero@163.com>
2023-12-06 23:27:40 +0800

    Add prettier config

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "printWidth": 120,
+  "semi": true,
+  "bracketSpacing": true,
+  "arrowParens": "avoid",
+  "trailingComma": "all",
+  "tabWidth": 2,
+  "endOfLine": "lf",
+  "singleQuote": true
+}
